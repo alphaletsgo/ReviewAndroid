df23af2241f04c25bb6fb60ad0683d38da629228
zhy <zhy_ho@126.com>
2020-09-03 22:40:20 +0800

    抓取内容测试

--- backservice/src/main/java/cn/isif/backservice/MonitorService.java
+++ backservice/src/main/java/cn/isif/backservice/MonitorService.java
@@ -1,10 +1,15 @@
 package cn.isif.backservice;
 
 import android.accessibilityservice.AccessibilityService;
+import android.os.Build;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
+import android.view.accessibility.AccessibilityWindowInfo;
+
+import androidx.annotation.RequiresApi;
 
 import java.util.List;
 
@@ -22,32 +27,42 @@ public class MonitorService extends AccessibilityService {
         super.onServiceConnected();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
-        switch (accessibilityEvent.getAction()) {
-            case AccessibilityEvent.TYPE_VIEW_CLICKED:
-            case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:
-                Log.d(TAG, "CLICK EVENT");
-                break;
-            default:
-                Log.d(TAG,"" + accessibilityEvent.getAction());
-                break;
-        }
-//        Log.d(TAG, "onAccessibilityEvent: " + accessibilityEvent.toString());
-//        if (!TextUtils.isEmpty(accessibilityEvent.getPackageName())) {
-//            Log.d(TAG + "-" + "packageName", accessibilityEvent.getPackageName().toString());
-//        }
-//        if (!TextUtils.isEmpty(accessibilityEvent.getClassName())) {
-//            Log.d(TAG + "-" + "className", accessibilityEvent.getClassName().toString());
-//        }
+        Log.d(TAG, "onAccessibilityEvent");
+        //获取Root
 //        AccessibilityNodeInfo rootNode = getRootInActiveWindow();
-//        if (null != rootNode) {
-//
-//        }
-//        List<CharSequence> textTree = accessibilityEvent.getText();
-//        for (CharSequence cs : textTree) {
-//            Log.d(TAG, cs.toString());
-//        }
+//        printChild(rootNode);
+        List<AccessibilityWindowInfo> windowInfos = getWindows();
+        for (AccessibilityWindowInfo awi : windowInfos) {
+            AccessibilityNodeInfo nodeInfo = awi.getRoot();
+            if (null != nodeInfo) {
+                printChild(nodeInfo);
+            }
+        }
+    }
+
+    private void printChild(AccessibilityNodeInfo node) {
+        int count = node.getChildCount();
+        for (int i = 0; i < count; i++) {
+            AccessibilityNodeInfo child = node.getChild(i);
+            if (null != child) {
+                //两种获取text的方式
+                CharSequence text = child.getText();
+                if (TextUtils.isEmpty(text)) {
+                    text = child.getContentDescription();
+                }
+                if (!TextUtils.isEmpty(text)) {
+                    Log.d(TAG, text.toString());
+                }
+                if (child.getChildCount() == 0) {
+                    return;
+                } else {
+                    printChild(child);
+                }
+            }
+        }
     }
 
     @Override
